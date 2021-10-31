beda9200090efc544883907772358155de3ed6f0
YourUserName <Your@Email.address>
2021-10-31 18:57:05 +0400

    step 1

--- plots_cli.py
+++ plots_cli.py
@@ -32,6 +32,6 @@ def main():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-   ('PyCharm')
+     ('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
