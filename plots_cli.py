33bfab0cc2082bf91e33a6c56c486a8ef8ffd1b2
YourUserName <Your@Email.address>
2021-10-31 18:45:50 +0400

    step 1

---
+++ plots_cli.py
@@ -0,0 +1,23 @@
+import numpy as np
+import csv
+import plotter
+
+
+def main():
+
+    def quits():
+        sure = input
+        while sure == 'y':
+            print('Goodbye!')
+            return True
+        else:
+            return False
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
