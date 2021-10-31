cdb614542ddf750568e13939bb78d5574fac2b7d
YourUserName <Your@Email.address>
2021-10-31 18:56:16 +0400

    step 1

--- plots_cli.py
+++ plots_cli.py
@@ -5,19 +5,33 @@ import plotter
 
 def main():
 
-    def quits():
-        sure = input
-        while sure == 'y':
-            print('Goodbye!')
-            return True
-        else:
-            return False
-
-
+              #quit function
+        def quits():
+                sure = input("are you sure?")
+                while sure == 'y':
+                    print('goodbye!')
+                    return True
+                while sure == 'Y':
+                    print('goodbye!')
+                    return True
+                else:
+                    return False
+        def bye():
+            user = input(">>")
+            string = user.split()
+            cmd = string[0]
+            if cmd == "quit":
+                    while quits() == True:
+                        break
+            elif cmd == 'stu':
+                    try:
+                        if len(string) != 4:
+                            print('incorrect usage, "Usage: stu <filename> <last name> <first name>"')
+                        else:
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+   ('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
