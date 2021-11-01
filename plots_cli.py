c62c6e689681b4a5e224d61c939e35670ecab50a
YourUserName <Your@Email.address>
2021-11-01 18:55:59 +0400

    update ?

--- plots_cli.py
+++ plots_cli.py
@@ -10,14 +10,14 @@ def main():
 
 
     if (z[0] == "stu"):
-        student_average(["stu", r"C:\Users\talaa\Downloads\GCIS.123.600-assignment2-sample (1).csv", "Elsawi", "Talaat" , students_average_plotting()])
+        student_average(["stu", r"C:\Users\talaa\Downloads\GCIS.123.600-assignment2-sample (1).csv", ()] )
 
     if (command == "avg"):
         print_average(["avg",r"C:\Users\talaa\Downloads\GCIS.123.600-assignment2-sample (1).csv", 100])
 
 
     if (command == "cavg"):
-          class_average(["cavg", r"C:\Users\talaa\Downloads\GCIS.123.600-assignment2-sample (1).csv"])
+          class_average(["cavg", r"C:\Users\talaa\Downloads\GCIS.123.600-assignment2-sample (1).csv"] )
 
 
     if (command == "help"):
@@ -73,39 +73,19 @@ def bye(quits):
             else:
                 print('invalid command')
 
-def student_average(stu_string, plot, userfile):
-  # calculates and plots
-    stu_string = ["stf",r"C:\Users\talaa\Downloads\GCIS.123.600-assignment2-sample (1).csv"]
-    csv_reader = csv.reader(userfile)
-    firstname = stu_string[2]
-    lastname= stu_string[3]
-    row = []
-    for row in csv_reader:
-        while firstname in row:
-            if lastname in row:
-                print(row)
-                plotter.init("my graph", "X-axis", "Y-axis")
-                plotter.new_series()
-                for i in range(len(plot)):
-                    plotter.add_data_point(plot[i])
-                    plotter.plot()
-                    plotter.plot()
-                if  plotter.plot() == True:
-                    print('Plotting complete. (window may be hidden)')
-                break
-            elif    plotter.plot() == False:
-                print('Plot faild. (student not found)')
-                break
-
-        #try:
-
-             #print('name not found in csv file')
-
-   # try:
-
-
-      #print('file not found')
-
+def student_average(csv):
+    f = open(csv,r"C:\Users\talaa\Downloads\GCIS.123.600-assignment2-sample (1).csv", )
+    average = 0
+    Sum = 0
+    row_count = 0
+    for row in f:
+        for column in row.split(','):
+            n=float(column)
+            Sum += n
+        row_count += 1
+    average = Sum / len(column)
+    f.close()
+    return 'The average is:', average
 
 
 def print_average(avg,):
@@ -122,22 +102,36 @@ def print_average(avg,):
         average = Sum / 12
         print(average)
 
-def class_average(args):
-    filename = args[1]
-    try:
-        with open(filename, 'r') as f:
-            csv_reader = csv.reader(f)
-            next(csv_reader)
-            plotter.init("Class averrage", "Grade item", "Grade")
-            for i in range(1, 11):
-                plotter.add_data_point(round(print_average(["avg", "points.csv", i])[0], 2))
-                plotter.plot()
-                return True
-    except FileNotFoundError:
-        print(f'[ERROR] No such file: {filename}')
-        return -1
-    except:
-        return -1
+def class_average(class_string, data_points, dot_color):
+  # plots multiple averages for multiple students
+    class_string = ["cavg", "filename", plotter.plot_data_points(data_points, dot_color, trace_plot=True)]
+
+    with open(r"C:\Users\Zeyad\GCIS.123.600-assignment2-sample.csv") as namefile:
+       csv_reader = csv.reader(namefile)
+       print(csv_reader)
+       fisrtname = class_string[1]
+       lastname = class_string[2]
+    row = []
+    for row in csv_reader:
+           while fisrtname in row:
+               if lastname in row:
+                   print(row)
+                   plotter.init("my graph", "X-axis", "Y-axis")
+
+    if len(class_string) != 2:
+        print("Usage: cavg <filename>")
+
+        try:
+            if (class_string == True):
+                print("Plot is finished (window may be hidden).")
+
+            if (class_string[0] == False):
+                print("Usage: cavg <filename>")
+            if (class_string[1] == False):
+                print("No such file: foo.csv")
+
+        except:
+            return
 
 
 def help():
@@ -148,20 +142,7 @@ def help():
     print("quit - quits")
     print("help - displays this message")
 
-def students_average_plotting():
-    plotter.init("my graph", "X-axis", "Y-axis")
-    plotter.add_data_point(0.00)
-    plotter.add_data_point(82)
-    plotter.add_data_point(13.37)
-    plotter.add_data_point(28.72)
-    plotter.add_data_point(50.00)
-    plotter.add_data_point(90.65)
-    plotter.add_data_point(11.06)
-    plotter.add_data_point(34.43)
-    plotter.add_data_point(95)
-    plotter.add_data_point(75)
-    plotter.plot(trace_plot=True)
-    input("input anything to exit")
+
 
 
 def print_average_calculation():
@@ -174,5 +155,4 @@ def print_average_calculation():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
      main()
-
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
