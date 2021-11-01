5f0e26395b60effea990020a194efc51ffc2b928
YourUserName <Your@Email.address>
2021-11-01 14:10:36 +0400

    test 2

--- test.py
+++ test.py
@@ -11,4 +11,12 @@ def test_student_average():
     assert plots_cli.student_average(args_false) == False
 
 
+def test_print_average():
+    "calculates hard coded values"
 
+    args = ["avg", "points.csv", 1]
+    args_fail = ["avg", "points.csv", 11]
+    data = [12.67, 16.67, 14.0, 16.67, 16.0, 0, 8.0, 18.67, 14.7, 14.67, 10.0, 12.67, 13.33, 8.67, 15.33, 15.33, 14.67, 13.33, 7.33, 16.00, 17.33, 19.33, 15.33, 0, 6.67, 15.33, 18.0, 16.67, 13.33, 15.33, 15.33, 16.0, 14.0]
+    avg = sum(data) / len(data)
+    assert plots_cli.print_average(args)[0] == avg
+    assert plots_cli.print_average(args_fail) == -1
