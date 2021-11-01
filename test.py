2b3b019eed1e920639353a5bbd24158edff3c583
YourUserName <Your@Email.address>
2021-11-01 14:00:21 +0400

    test 1

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
 import plots_cli
-def test_student_average(["stu",r"C:\Users\talaa\Downloads\GCIS.123.600-assignment2-sample (1).csv", "Elsawi", "Talaat"]):
+
+def test_student_average():
+    "  Tests three command inputs and their expected output "
+
+    args_true = ["stu", "points.csv", "Talaat", "Elsawi"]
+    args_none = ["stu", "point.csv", "Kashyap", "kyle Bene"]
+    args_false = ["stu", "points.csv", "Kashyap", "'kyle Bene"]
+    assert plots_cli.student_average(args_true) == True
+    assert plots_cli.student_average(args_none) == None
+    assert plots_cli.student_average(args_false) == False
+
+
+
